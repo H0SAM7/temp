ec64935557d86624594fda1bfee31ba268698dff
hosam <hoadel2003@gmail.com>
2024-12-24 13:50:05 +0200

    first

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color whiteColor =  Colors.white;
+Color blackColor =  Colors.black;
+Color gray100 = const Color(0xffF5F5F5);
+Color gray500 = const Color(0xff6B6B6B);
+Color blue500 = const Color(0xff3B82F6);
+Color blue100= const Color(0xffBFDBFE);
